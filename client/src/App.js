43772e1a199437d174ea14d6845ac1a60a1edd2e
amebrahimi <amirebrahimi5@live.com>
2018-11-27 13:37:00 +0330

    Add Education Form (Video 57)

--- client/src/App.js
+++ client/src/App.js
@@ -62,6 +62,7 @@ class App extends Component {
                                 <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
                                 <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
                                 <PrivateRoute exact path="/add-experience" component={AddExperience}/>
+                                <PrivateRoute exact path="/add-education" component={AddEducation}/>
                             </Switch>
                         </div>
                         <Footer/>
