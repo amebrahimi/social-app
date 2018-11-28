2a8061ef71aae31a180096ae02bed58ddc494867
amebrahimi <amirebrahimi5@live.com>
2018-11-28 17:25:40 +0330

    Posts Post Form Component (Video 67)

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -68,6 +69,7 @@ class App extends Component {
                                 <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
                                 <PrivateRoute exact path="/add-experience" component={AddExperience}/>
                                 <PrivateRoute exact path="/add-education" component={AddEducation}/>
+                                <PrivateRoute exact path="/feed" component={Posts}/>
                             </Switch>
                             <Route exact path="/notfound" component={NotFound}/>
                         </div>
